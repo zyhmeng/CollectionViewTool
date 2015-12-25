971ddc27a57f5967a23b2a4162a93692a740507b
zhangyuhao <zhangyuhao1158@sina.com>
2015-12-25 10:08:53 +0800

    Initial Commit

---
+++ CollectionViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollectionViewDemo
+//
+//  Created by zyh on 15/12/25.
+//  Copyright © 2015年 zyh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
